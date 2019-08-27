ca4060075827f4a61b59f0097efff998af08eb1e
mdhamija94 <mdhamija94@gmail.com>
2019-08-27 13:33:17 -0700

    webpack is working

---
+++ widgets/frontend/entry_file.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let root = document.getElementById('root');
+  ReactDOM.render(<h1>REACT IS WORKING</h1>, root)
+});
